64a98823347cde3236c5e58dedfab880a96bc944
Rafael Leal Pardo <falilealp@gmail.com>
2024-03-25 18:50:01 +0100

    one more exercise and two pending

--- Binary3.cpp
+++ Binary3.cpp
@@ -82,7 +82,43 @@ bool isABB(const Abin<tValue> &abin, const typename Abin<tValue>::nodo node){
 template <typename tValue>
 bool abinIsABB(const Abin<tValue> &abin){
     if(abin.arbolVacio()) return true;
-    else isABB(abin,abin.raiz());
+    else return isABB(abin,abin.raiz());
 }
+
+#pragma endregion
+
+/* Exercise 4: Implements an applet that, given a binary tree, counts the number of nodes that have more than 4 descendants. */
+#pragma region Exercise4
+
+template <typename tValue>
+unsigned int numberOfDescendants(const Abin<tValue> &abin, const typename Abin<tValue>::nodo node){
+    if(node == abin.NODO_NULO) return 0;
+    else return 1 + numberOfDescendants(abin,abin.hijoIzqdo(node)) + numberOfDescendants(abin,abin.hijoDrcho(node));
+}
+
+template <typename tValue>
+unsigned int moreThanFour(const Abin<tValue> &abin, const typename Abin<tValue>::nodo node){
+    if(node == abin.NODO_NULO) return 0;
+    else if(numberOfDescendants(abin,node) >= 4) return 1 + moreThanFour(abin,abin.hijoIzqdo(node)) + moreThanFour(abin,abin.hijoDrcho(node));
+    else return moreThanFour(abin,abin.hijoIzqdo(node)) + moreThanFour(abin,abin.hijoDrcho(node));
+}
+
+template <typename tValue>
+unsigned int moreThanFourDescendants(const Abin<tValue> &abin){
+    if(abin.arbolVacio()) return 0;
+    else return moreThanFour(abin,abin.raiz());
+}
+
+#pragma endregion
+
+/* Exercise 5: Construct a function that, given a Binary Tree, returns true if it is an AVL and false otherwise. */
+#pragma region Exercise5
+
+#pragma endregion
+
+/* Exercise 6: Implements a generic function that transforms a binary tree of a generic type tValue, eliminating the proper 
+descendants of all those nodes whose content be, at the same time greater than or equal to his own ancestors and 
+less than or equal to his own descendants */
+#pragma region Exercise6
 
 #pragma endregion
